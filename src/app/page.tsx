855cc0e90167b8560972dfe27a691d6506779517
Natalie Stam <natalielynnstam@gmail.com>
2024-01-24 19:57:02 -0800

    mobile view & page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
-const Home = () => (
-  <main className="flex items-center">
-    <div>hello!!</div>
-  </main>
-)
+import type { ReactNode } from 'react';
 
-export default Home;
+const Section = ({ id, header, children }: { id: string; header: string; children: ReactNode }) => (
+  <div id={id} className="h-screen flex flex-col justify-center">
+    <h2 className="text-6xl">{header}</h2>
+    {children}
+  </div>
+);
+
+
+const Page = () => (
+  <>
+    <Section id="about" header="👩🏻‍💻hi, i&apos;m natalie.">
+      <p>i&apos;m a full-stack engineer based in oakland, california.</p>
+    </Section>
+    <Section id="resume" header="resume">
+      <p>coming soon.</p>
+    </Section>
+    <Section id="contact" header="contact me">
+      <p>coming soon.</p>
+    </Section>
+  </>
+);
+
+export default Page;
